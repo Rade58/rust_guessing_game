8fcd28cfd61390f0e2ee930527412abb4e294b84
Rade58 <bajic.rade2@gmail.com>
2022-11-04 16:30:15 +0100

    🦉

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ fn main() {
     //
     let secret_number = rand::thread_rng().gen_range(1..=100); //
 
-    println!("secret number: {secret_number}");
+    // println!("secret number: {secret_number}");
 
     loop {
         println!("Input your guess!");
